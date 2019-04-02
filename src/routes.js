1d8385d06eececc9e79f088a8b3aa1ea32ea7b82
Bernabé González <gobernabe@gmail.com>
2019-04-02 11:09:14 -0300

    Homepage for test suites (#9)

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import routesPaths from 'constants/routesPaths'
-import HomePage from 'containers/HomePage'
+import HomePage from 'containers/HomePage2'
 import NotFoundPage from 'containers/NotFoundPage'
 
 const routes = [
   {
     path: routesPaths.index,
     component: HomePage,
-    exact: true,
-    private: true
+    exact: true
   },
   {
     component: NotFoundPage
---
+++ src/state/schemas/testSuite.js
@@ -0,0 +1,8 @@
+import { schema } from 'normalizr'
+import normalize from './normalize'
+
+const testSuite = new schema.Entity('testSuites')
+
+export const testSuites = new schema.Array(testSuite)
+
+export const normalizeData = data => normalize(data, testSuites)
