796697469b75d7182defd2072c2314b9a4b25878
Bernabe Gonzalez <bernabe@rootstrap.com>
2019-04-03 11:30:48 -0300

    Create suite

---
+++ src/containers/CreateTestSuite/TestsPicker.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import { Button, Modal, ModalClose, ModalContent } from 'mc-components'
+
+import api from 'api'
+import TreeView from 'components/TreeView'
+import Code from 'components/Code'
+
+const TestsPicker = ({ onAddTest, onRemoveTest, selectedTests }) => {
+  const [structure, setStructure] = useState()
+  const [cursor, setCursor] = useState()
+  const [showCode, setShowCode] = useState(false)
+  const [loadingCode, setLoadingCode] = useState(false)
+
+  useEffect(() => {
+    api('/folders').then(setStructure)
+  }, [])
+
+  const onToggle = node => {
+    if (node.type === 'file') {
+      setCursor(node)
+    }
+  }
+
+  const onToggleCode = () => {
+    if (!showCode) {
+      setLoadingCode(true)
+    }
+    setShowCode(!showCode)
+  }
+
+  const onCodeLoaded = () => setLoadingCode(false)
+  const isSelected = cursor && selectedTests.has(cursor.path)
+
+  return (
+    <div className="row">
+      <div className="col-5">
+        {structure && (
+          <TreeView
+            data={structure}
+            onToggle={onToggle}
+            activeNode={cursor && cursor.path}
+            isExpanded
+          />
+        )}
+      </div>
+      {cursor && (
+        <div className="col-7">
+          <div>
+            <span className="mc-text-h5 mc-mb-3 mc-mr-3">{cursor.name}</span>
+            {isSelected ? (
+              <Button
+                kind="secondary"
+                onClick={() => onRemoveTest(cursor.path)}
+              >
+                Remove test
+              </Button>
+            ) : (
+              <Button kind="primary" onClick={() => onAddTest(cursor.path)}>
+                Add test
+              </Button>
+            )}
+            <div className="mc-mt-5">
+              <Button
+                kind="tertiary"
+                className="mc-mb-4"
+                onClick={onToggleCode}
+                loading={loadingCode}
+              >
+                Show source code
+              </Button>
+              <Modal onClose={onToggleCode} show={showCode}>
+                <ModalClose />
+                <div className="container">
+                  <ModalContent>
+                    <div className="mc-my-10">
+                      <Code test={cursor.path} onCodeLoaded={onCodeLoaded} />
+                    </div>
+                  </ModalContent>
+                </div>
+              </Modal>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+TestsPicker.propTypes = {
+  onAddTest: PropTypes.func.isRequired,
+  onRemoveTest: PropTypes.func.isRequired,
+  selectedTests: PropTypes.object.isRequired
+}
+
+export default TestsPicker
