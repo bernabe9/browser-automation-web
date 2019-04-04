cb2cff3ba8aa4a4d670d53dee39eed7f2714f544
Schubert <stf9331@gmail.com>
2019-04-04 15:33:02 -0300

    Add support for run in custom url. Remove requirement props.

--- src/containers/SuitePage/SuitePage.js
+++ src/containers/SuitePage/SuitePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Separator, Button } from 'mc-components'
+import { Separator, FormGroup, Input, Button } from 'mc-components'
 
 import api from 'api'
 import Header from 'components/Header'
@@ -8,6 +8,8 @@ import { applyQueryParams } from 'utils/helpers'
 import SuiteTest from './SuiteTest'
 
 const SuitePage = ({ fetchSuite, suite }) => {
+  const [inputURL, setInputURL] = useState(false)
+  const [url, setUrl] = useState('')
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
@@ -16,50 +18,71 @@ const SuitePage = ({ fetchSuite, suite }) => {
 
   const handleRunSuite = () => {
     setLoading(true)
-    api(applyQueryParams('/run-suite', { suite: suite.id })).then(() => {
+    api(applyQueryParams('/run-suite', { suite: suite.id, url })).then(() => {
       setLoading(false)
       fetchSuite()
     })
   }
 
-  const handleRunSingleTest = path => () => {
+  const handleRunSingleTest = (path, url) => () => {
     setLoading(true)
-    api(applyQueryParams('/run-suite', { suite: suite.id, test: path })).then(
-      () => {
-        setLoading(false)
-        fetchSuite()
-      }
-    )
+    api(
+      applyQueryParams('/run-suite', { suite: suite.id, test: path, url })
+    ).then(() => {
+      setLoading(false)
+      fetchSuite()
+    })
   }
 
   return (
     <div>
       <Header />
-      <div className="container mc-mt-5 mc-p-5 mc-invert mc-background--color-light">
-        <div className="row align-items-center">
-          <div className="col-auto">
-            {!!suite && <h5 className="mc-text-h5">{suite.name}</h5>}
+      {suite && suite.tests && (
+        <div className="container mc-mt-5 mc-p-5 mc-invert mc-background--color-light">
+          <div className="mc-mb-4">
+            <h5 className="mc-text-h5 mc-text--uppercase">{suite.name}</h5>
+            <p>{suite.description}</p>
+            <p className="mc-text--hinted">{suite.url}</p>
           </div>
-          <div className="col-auto">
-            <Button onClick={handleRunSuite} loading={loading}>
-              Run Entire suite
-            </Button>
-          </div>
-        </div>
-        <Separator />
-        <div className="mc-py-4">
-          {suite &&
-            suite.tests &&
-            Object.values(suite.tests).map(test => (
+          <FormGroup name="url">
+            <div className="row align-items-center">
+              <div className="col-auto">
+                <Button onClick={handleRunSuite} loading={loading}>
+                  Run Entire suite
+                </Button>
+              </div>
+              <div className="col-12 col-sm-3">
+                {inputURL ? (
+                  <Input
+                    onChange={e => setUrl(e.target.value)}
+                    value={url}
+                    placeholder={suite.url}
+                  />
+                ) : (
+                  <a
+                    className="mc-text-h8 mc-text--uppercase mc-text--muted"
+                    onClick={() => setInputURL(true)}
+                  >
+                    Change url
+                  </a>
+                )}
+              </div>
+            </div>
+          </FormGroup>
+          <Separator />
+          <div className="mc-py-4">
+            {Object.values(suite.tests).map(test => (
               <SuiteTest
                 key={test.path}
                 loading={loading}
                 test={test}
+                defaultUrl={suite.url}
                 handleRunSingleTest={handleRunSingleTest}
               />
             ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
@@ -67,9 +90,9 @@ const SuitePage = ({ fetchSuite, suite }) => {
 SuitePage.propTypes = {
   fetchSuite: PropTypes.func.isRequired,
   suite: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    tests: PropTypes.object.isRequired
+    id: PropTypes.string,
+    name: PropTypes.string,
+    tests: PropTypes.object
   })
 }
 
