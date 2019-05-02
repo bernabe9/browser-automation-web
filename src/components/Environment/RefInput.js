b8f2b12ded44d217dfc0ca9b5eb5c66457cde7a2
stabarez <stf9331@gmail.com>
2019-05-02 14:25:51 -0300

    [BA-27] Add button to change reference (#34)

---
+++ src/components/Environment/RefInput.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Input, FormGroup } from 'mc-components'
+
+import Anchor from 'components/Anchor'
+
+const RefInput = ({ defaultRef, enabled, repository, onToggle, onChange }) => {
+  return (
+    <div className="row">
+      <div className="col-3">
+        <Anchor onClick={onToggle}>
+          {enabled ? 'use current reference' : 'change reference'}
+        </Anchor>
+        {enabled && (
+          <div className="mc-mt-4">
+            <FormGroup label="Ref" name="ref">
+              <Input
+                onChange={e => onChange(e.target.value)}
+                value={repository}
+                placeholder={defaultRef}
+              />
+            </FormGroup>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+RefInput.propTypes = {
+  defaultRef: PropTypes.string,
+  enabled: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  repository: PropTypes.string
+}
+
+export default RefInput
