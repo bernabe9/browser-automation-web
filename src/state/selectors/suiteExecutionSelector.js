061cc1a3be425ea9a86abc1d5a0ef19c62460a10
Schubert <stf9331@gmail.com>
2019-05-14 14:50:21 -0300

    [BA-41] Order executions by status

--- src/state/selectors/suiteExecutionSelector.js
+++ src/state/selectors/suiteExecutionSelector.js
@@ -5,4 +5,24 @@ export default class SuiteExecutionSelector extends Selector {
   objName = 'suiteExecutions'
 
   schema = suiteExecutionSchema
+
+  findOrdered = (state, fn) => {
+    const suite = this.find(state, fn)
+    if (suite) {
+      suite.executions.sort((a, b) => {
+        if (a.status === b.status) {
+          return 0
+        }
+        if (
+          a.status === 'error' ||
+          b.status === 'pending' ||
+          (a.status === 'running' && b.status !== 'error')
+        ) {
+          return -1
+        }
+        return 1
+      })
+    }
+    return suite
+  }
 }
