d7a2a850b2ae3e96ef770c8029777ca37b9e29d4
Schubert <stf9331@gmail.com>
2019-08-15 16:33:26 -0300

    [BA-114] Remove tests from suites

--- src/components/SelectedTests/index.js
+++ src/components/SelectedTests/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Flex from 'components/Flex'
+import Anchor from 'components/Anchor'
 
-const SelectedTests = ({ selectedTests, showError }) => (
+const SelectedTests = ({ onRemoveTest, selectedTests, showError }) => (
   <div className="mc-mb-3">
     <p className="mc-text-h8 mc-mb-1">Selected tests</p>
     {selectedTests.size === 0 ? (
       <p>No tests selected yet</p>
     ) : (
-      Array.from(selectedTests).map(test => <p key={test}>{test}</p>)
+      Array.from(selectedTests).map(test => (
+        <Flex key={test}>
+          <p className="mc-mr-2">{test}</p>
+          <Anchor onClick={() => onRemoveTest(test)}>Remove</Anchor>
+        </Flex>
+      ))
     )}
     {showError && (
       <p className="mc-text--error">You must select at least one test</p>
@@ -16,6 +23,7 @@ const SelectedTests = ({ selectedTests, showError }) => (
 )
 
 SelectedTests.propTypes = {
+  onRemoveTest: PropTypes.func.isRequired,
   selectedTests: PropTypes.object.isRequired,
   showError: PropTypes.bool
 }
